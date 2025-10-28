c8bbf56324db76a4ccfb07526dfc762226fa33b6
Avinesh sharma <142591660+avineshsharma@users.noreply.github.com>
2025-10-28 14:25:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age){
+		this._name = name;
+		this._age = age;
+		
+	}
+	get name(){
+		return this._name;
+	}
+	set age(newage){
+		this._age = newage;
+	}
+}
 
 class Student extends Person {}
 
